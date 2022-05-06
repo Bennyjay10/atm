6a5a86f133145a77a77d90bbe7cc12da3451e1ff
benjay <benjamitunu@gmail.com>
2022-05-06 02:39:21 +0100

    second commit

---
+++ atm.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+       int pin;
+       int  balance = 30000,deposit,withdraw,balance_check,input,balance_withdraw;
+       int choice,option,option_opt,opt,opts;
+       int another_transaction; 
+      
