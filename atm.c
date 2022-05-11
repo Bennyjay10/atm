8c4bc72e9b4d41fe691c40a745a6b60f0b2562e5
benjay <benjamitunu@gmail.com>
2022-05-11 01:42:37 +0100

    BANKING SYSTEM

--- atm.c
+++ atm.c
@@ -1,23 +1,41 @@
 #include<stdio.h>
+#include<unistd.h>
 #include"atm.h"
-   int transaction(){
+  int pin;
+int option;
+int option_opt;
+int input;
+int balance=20000;
+int deposit;
+int another_transaction;
+int new_pin;
+int transact();
+   int transaction(){  
       
              printf("\t\t#======*YOU ARE HIGHLY WELCOME TO BENNYJAY'S BANKING PLC*======# \n");
+             sleep(3);
+             
+             //PIN = 2410
            while(pin!=2410){
                  printf("\n KINDLY INPUT YOUR PIN : ");
                  scanf("%d",&pin);
+                 sleep(3);
             if(pin == 2410){
                 printf("SUCCESSFUL!!!");
+                sleep(3);
             }else{
                  printf("INVALID PIN!!!");
             }
                }
                printf("\n\t\t\t\t#======*AVAILABLE TRANSACTIONS*======#");
+               sleep(3);
                printf("\n\n\t\t1.Withdrawal");
                printf("\n\n\t\t2.Deposit");
                printf("\n\n\t\t3.Balance Check");
+               printf("\n\n\t\t4.Change Pin");
              printf("\n\n\t\tPlease select any option: ");
                scanf("%d",&option);
+               sleep(3);
               
                switch (option)
                {
@@ -25,7 +43,7 @@
                do{
                     printf(">>SAVINGS(1)");
                 printf("\n>>CURRENT(2)");
-               printf("\n Select from option");
+               printf("\n Select from option :");
                  scanf("%d",&option_opt);
                  
                   } while(option_opt > 2 );
@@ -37,22 +55,22 @@
                 printf("\n>>1000(1)");printf("\n\t\t\t>>2000(2)");
                 printf("\n>>5000(3)");printf("\n\t\t\t>>10000(4)");
                 printf("\n>>15000(5)");
-                            scanf("%d",&opt);
-                           if (opt == 1){
+                            scanf("%d",&input);
+                           if (input == 1){
                                     printf("DEAR CUSTOMER YOU'VE SUCCESSFULLY WITHDRAWN 1000");
                                     
                            }
-                      else if(opt == 2){
+                      else if(input == 2){
                           printf("YOU'VE SUCCESSFULLY WITHDRAWN 2000");
                       }
-                       else if(opt == 3){
+                       else if(input == 3){
                           printf("YOU'VE SUCCESSFULLY WITHDRAWN 5000");
                  }
-                          else if(opt == 4){
+                          else if(input == 4){
                           printf("YOU'VE SUCCESSFULLY WITHDRAWN 10000");
                           }
                            
-                              else if(opt == 5){
+                              else if(input == 5){
                           printf("YOU'VE SUCCESSFULLY WITHDRAWN 15000");
                               }
                                 }
@@ -94,8 +112,16 @@
                 }
                
                   break;
-                  
-                  
+                   case 4:
+                   transact(pin,new_pin);
+                   
+                     //request for another transaction
+                                 printf("\n\n\t\tDo you want another transaction? \n Press 1 to proceed and 2 to exit \n\n");
+                scanf("%d",&another_transaction);
+                if(another_transaction==1){
+                    //call our transaction method here
+                    transaction();
+                }
                }
                
 
@@ -103,9 +129,12 @@
             
                }
 
+
        
 int main(){
        transaction();
+       
+
            
     return 0;
 }
--- atm.h
+++ atm.h
@@ -1,7 +1,34 @@
 #include<stdio.h>
+ int transact(int pin,int new_pin){
+                    printf("\n KINDLY INPUT YOUR CURRENT PIN : ");
+                 scanf("%d",&pin);
+                 sleep(3);
+          while(pin!=2410){
+                if(pin == 2410){
+                printf("SUCCESSFUL!!!");
+            }else{
+                 printf("INVALID PIN!!!");
+            }
+            } 
+               
+              while(new_pin < 1000 || new_pin > 9999){
+                      printf("\n KINDLY INPUT A NEW PIN : ");
+                     scanf("%d",&new_pin);
+                     sleep(3);
+               if(new_pin > 1000 && new_pin < 9999){
+                   printf("PIN HAS BEEN SUCCESSFULLY CHANGED");
+                   sleep(2);
+                        printf("\n DEAR CUSTOMER YOUR NEW PIN IS %d",new_pin);
+                        
+               }
+               else{
+                   printf("INPUT A VALID FOUR DIGIT PIN");
+               }
+              }
+           return(new_pin);
 
-       int pin;
-       int  balance = 30000,deposit,withdraw,balance_check,input,balance_withdraw;
-       int choice,option,option_opt,opt,opts;
-       int another_transaction; 
-      
+         
+ }
+
+
+ 
